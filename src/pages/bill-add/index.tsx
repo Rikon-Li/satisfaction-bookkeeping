976e7455072bc771102ec36caecdae2dcde31a1e
Ligen <linhu.lg@alibaba-inc.com>
2024-10-27 22:31:33 +0800

    feat: add bill add and list pages

---
+++ src/pages/bill-add/index.tsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: Ligen linhu.lg@alibaba-inc.com
+ * @Date: 2024-10-27 22:01:26
+ * @LastEditors: Ligen linhu.lg@alibaba-inc.com
+ * @LastEditTime: 2024-10-27 22:30:16
+ * @FilePath: /satisfaction-bookkeeping/src/pages/bookkeeping-add/index.tsx
+ * @Description: 账单添加
+ */
+import React from "react";
+
+function BillAdd() {
+  return <div>BillAdd</div>;
+}
+
+export default BillAdd;
---
+++ src/pages/bill-list/index.tsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: Ligen linhu.lg@alibaba-inc.com
+ * @Date: 2024-10-27 22:01:21
+ * @LastEditors: Ligen linhu.lg@alibaba-inc.com
+ * @LastEditTime: 2024-10-27 22:30:35
+ * @FilePath: /satisfaction-bookkeeping/src/pages/bill-list/index.tsx
+ * @Description: 账单列表
+ */
+import React from "react";
+
+function BillList() {
+  return <div>BillList</div>;
+}
+
+export default BillList;
